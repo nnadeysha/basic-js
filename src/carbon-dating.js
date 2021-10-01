bb922f6c64f13526fd4888aed0a6b0c0f012b1b0
nadeysha <nadeysha@mail.ru>
2021-10-01 15:47:23 +0400

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string' || !sampleActivity) return false;
+  sampleActivity = parseFloat(sampleActivity);
+  if(sampleActivity > 0 && sampleActivity < 15){
+    let result = Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD);
+    return Math.ceil( result)
+  } else {
+    return false
+  }
+  
+  
+
 }
