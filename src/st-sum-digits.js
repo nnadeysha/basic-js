8ff97b4fc9dcbd708f6d4660ca6e2b8e70000fce
nadeysha <nadeysha@mail.ru>
2021-10-01 20:49:16 +0400

    feat: add digist solution

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -12,7 +12,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+  let N = n.toString().split('');
+  let sum = 0;
+  for(let i = 0; i < N.length; i++) {
+    sum += Number(N[i]);
+    if(sum > 9) {
+      sum = getSumOfDigits(sum);
+    }
+  }
+  return sum;
 }
